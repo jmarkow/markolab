6908def6626b814d349d8d1ec8100044996ce895
jmarkow <markowitzmeister@gmail.com>
2015-07-02 12:52:45 -0400

    Change to preview

--- markolab_preview.m
+++ markolab_preview.m
@@ -9,11 +9,14 @@ AX=[];
 AX(1)=subplot(3,1,1:2);
 [s,f,t]=zftftb_pretty_sonogram(AUDIO,AUDIO_FS,'filtering',300,'norm_amp',1,'clipping',[-3 2]);
 imagesc(t,f/1e3,s);axis xy;
+ylim([0 9]);
 colormap(jet);
+ylabel('Fs (kHz)');
 
 timevec=[1:size(DATA,1)]/DATA_FS;
 
 AX(2)=subplot(3,1,3);
 plot(timevec,DATA);
 linkaxes(AX,'x');
+xlabel('Time (s)');
 
