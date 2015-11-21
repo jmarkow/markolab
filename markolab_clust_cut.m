ac0ca0cb46d2862403c3cce107d22fb421d8e8bd
Jeff Markowitz <markowitzmeister@gmail.com>
2015-11-21 18:05:32 -0500

    Major update to cluster cut GUI

--- markolab_clust_cut.m
+++ markolab_clust_cut.m
@@ -1,6 +1,12 @@
-function [FIGNUM,LABELS,SELECTION,DIMS]=markolab_clust_cut(FEATURES,FEATURE_LABELS,CLUSTERFUN)
+function [FIGNUM,LABELS,SELECTION,DIMS]=markolab_clust_cut(FEATURES,FEATURE_LABELS,CLUSTERFUN,DISPLAY_LIM)
 % general purpose GUI for cluster cutting, pass in FEATURES, returns LABELS
+% TODO: subsample display
 
+disp('Complete selection by closting the clustering window...')
+
+if nargin<4 | isempty(DISPLAY_LIM)
+	DISPLAY_LIM=inf;
+end
 
 if nargin<3 | isempty(CLUSTERFUN)
 	CLUSTERFUN=@(x,n) kmeans(x,n);
@@ -18,6 +24,13 @@ if nargin<2 | isempty(FEATURE_LABELS)
 	end
 end
 
+if nsamples>DISPLAY_LIM
+	disp_idx=unique(round(linspace(1,nsamples,DISPLAY_LIM)));
+	disp([sprintf('Displaying %g out of %g total points)',length(disp_idx),nsamples)]);
+else
+	disp_idx=1:nsamples;
+end
+
 ndims=3;
 
 main_window=figure('Visible','off','Position',[360,500,700,600],'Name','Markolab Clust Cut','NumberTitle','off');
@@ -25,7 +38,7 @@ plot_axis=axes('Units','pixels','Position',[50,50,400,400]);
 
 pop_up_x= uicontrol('Style','popupmenu',...
 	'String',FEATURE_LABELS,...
-	'Position',[400,90,75,25],'call',@change_plot,...
+	'Position',[400,90,90,25],'call',@change_plot,...
 	'Value',1);
 pop_up_x_text= uicontrol('Style','text',...
 	'String','X',...
@@ -34,7 +47,7 @@ pop_up_x_text= uicontrol('Style','text',...
 
 pop_up_y= uicontrol('Style','popupmenu',...
 	'String',FEATURE_LABELS,...
-	'Position',[495,90,75,25],'call',@change_plot,...
+	'Position',[495,90,90,25],'call',@change_plot,...
 	'Value',min(nfeatures,2));
 pop_up_y_text= uicontrol('Style','text',...
 	'String','Y',...
@@ -42,7 +55,7 @@ pop_up_y_text= uicontrol('Style','text',...
 
 pop_up_z= uicontrol('Style','popupmenu',...
 	'String',FEATURE_LABELS,...
-	'Position',[595,90,75,25],'call',@change_plot,...
+	'Position',[595,90,90,25],'call',@change_plot,...
 	'Value',min(nfeatures,3));
 pop_up_z_text= uicontrol('Style','text',...
 	'String','Z',...
@@ -50,10 +63,9 @@ pop_up_z_text= uicontrol('Style','text',...
 
 pop_up_clusters= uicontrol('Style','popupmenu',...
 	'String',[1:9],...
-	'Position',[475,210,75,25],'value',1,...
-	'call',@change_cluster);
+	'Position',[475,210,75,25],'value',1);
 pop_up_clusters_text= uicontrol('Style','text',...
-	'String','Number of Clusters (will recluster)',...
+	'String','N(clusters) auto-clust',...
 	'Position',[500,250,100,45]);
 
 pop_up_choice= uicontrol('Style','popupmenu',...
@@ -65,24 +77,39 @@ pop_up_choice_text= uicontrol('Style','text',...
 
 push_draw_mode= uicontrol('Style','pushbutton',...
 	'String','Draw cluster (X and Y only)',...
-	'Position',[500,450,100,35],'value',0,...
+	'Position',[500,475,180,35],'value',0,...
+	'Call',@change_cluster);
+push_auto_mode = uicontrol('Style','pushbutton',...
+	'String','Auto-clust (all checked feat.)',...
+	'Position',[500,425,180,35],'value',0,...
 	'Call',@change_cluster);
 
 rows=ceil(length(FEATURE_LABELS)/5);
 
-feature_cluster_text= uicontrol('Style','text',...
-	'String','Cluster features',...
-	'Position',[200,550,100,45]);
+
+status_text= uicontrol('Style','text',...
+	'String','Status: Waiting for input...',...
+	'Position',[600,550,150,45]);
+
+
+nrows=ceil(length(FEATURE_LABELS)/5);
+ncolumns=5;
+
+feature_panel= uipanel('Parent',main_window,...
+	'Title','Auto-clust features',...
+	'Units','Pixels',...
+	'Position',[50 540-nrows*35 80*ncolumns 50*nrows]);
 
 i=1;
 while i<=length(FEATURE_LABELS)
 	row=ceil(i/5);
 	column=mod(i,5);
 	if column==0, column=5; end
-
-	cluster_dims{i}=uicontrol('Style','checkbox',...
+	cluster_dims{i}=uicontrol('parent',feature_panel,...
+		'Style','checkbox',...
 		'String',FEATURE_LABELS{i},...
-		'Value',0,'Position',[50+column*60,550-row*35,70,25]);
+		'Units','Normalized',...
+		'Value',0,'Position',[(column-1)*(.98/ncolumns),1-row*(1/nrows),1/ncolumns,1/nrows]);
 	set(cluster_dims{i},'Units','Normalized')
 	i=i+1;
 end
@@ -92,7 +119,7 @@ set(cluster_dims{1},'Value',1);
 % now align everything and send the main_window handle to the output
 
 align([pop_up_clusters,pop_up_clusters_text,...
-	pop_up_choice,pop_up_choice_text,push_draw_mode],'Center','None');
+	pop_up_choice,pop_up_choice_text,push_draw_mode,push_auto_mode,status_text],'Center','None');
 align([pop_up_x,pop_up_x_text],'Center','None');
 align([pop_up_y,pop_up_y_text],'Center','None');
 align([pop_up_z,pop_up_z_text],'Center','None');
@@ -108,7 +135,7 @@ set(pop_up_choice,'value',1);
 
 set([main_window,plot_axis,pop_up_x,pop_up_x_text,pop_up_y,pop_up_y_text,pop_up_z,...
 	pop_up_z_text,pop_up_clusters,pop_up_clusters_text,pop_up_choice,pop_up_choice_text,...
-	push_draw_mode,feature_cluster_text],'Units','Normalized');
+	push_draw_mode,push_auto_mode,status_text,feature_panel],'Units','Normalized');
 
 movegui(main_window,'center')
 set(main_window,'Visible','On')
@@ -134,19 +161,19 @@ colors=colormap(['lines(' num2str(clusternum) ')']);
 
 switch ndims
 	case 2
-		for i=1:clusternum	
+		for i=1:clusternum
 			clusterid{i}=num2str(i);
-			points=find(LABELS==clusters(i));
-			h(:,i)=plot(FEATURES(points,viewdim(1)),FEATURES(points,viewdim(2)),...
+			points=find(LABELS(disp_idx)==clusters(i));
+			h(:,i)=plot(FEATURES(disp_idx(points),viewdim(1)),FEATURES(disp_idx(points),viewdim(2)),...
 				'o','markerfacecolor',colors(i,:),'markeredgecolor','none');hold on
 		end
-	
+
 	case 3
 		for i=1:clusternum
 			clusterid{i}=num2str(i);
-			points=find(LABELS==clusters(i));
-			h(:,i)=plot3(FEATURES(points,viewdim(1)),FEATURES(points,viewdim(2)),FEATURES(points,viewdim(3)),...
-				'o','markerfacecolor',colors(i,:),'markeredgecolor','none');hold on
+			points=find(LABELS(disp_idx)==clusters(i));
+			h(:,i)=plot3(FEATURES(disp_idx(points),viewdim(1)),FEATURES(disp_idx(points),viewdim(2)),...
+			FEATURES(disp_idx(points),viewdim(3)),'o','markerfacecolor',colors(i,:),'markeredgecolor','none');hold on
 		end
 
 end
@@ -175,6 +202,9 @@ function change_cluster(varargin)
 %
 %
 
+disp('Updating clusters...');
+
+
 choices=get(pop_up_clusters,'string');
 clusternum=str2num(choices(get(pop_up_clusters,'value')));
 
@@ -183,6 +213,13 @@ viewdim(2)=get(pop_up_y,'value');
 viewdim(3)=get(pop_up_z,'value');
 
 draw_mode=get(push_draw_mode,'value');
+
+if draw_mode
+	set(status_text,'string','Status:  Updating clusters (draw mode)...');
+else
+	set(status_text,'string','Status:  Updating clusters (auto-clust)...')
+end
+drawnow;
 
 dim=[];
 for i=1:length(cluster_dims)
@@ -208,7 +245,6 @@ clusternum=str2num(choices(get(pop_up_clusters,'value')));
 
 LABELS=CLUSTERFUN(FEATURES(:,dim),clusternum);
 
-
 % clear the plot axis
 
 cla;
@@ -218,12 +254,13 @@ cla;
 
 if draw_mode
 
+	drawnow;
 	ndims=2;
-	plot(FEATURES(:,viewdim(1)),FEATURES(:,viewdim(2)),'o','markerfacecolor','b');view(2);
+	plot(FEATURES(disp_idx,viewdim(1)),FEATURES(disp_idx,viewdim(2)),'o','markerfacecolor','b');view(2);
 	hold on
 	disp('Select the corners of the enclosing polygon then press RETURN to continue...');
 	hold off;
-	
+
 	LABELS=ones(nsamples,1);
 	counter=2;
 
@@ -232,14 +269,15 @@ if draw_mode
 	while isempty(response2)
 
 		[xv,yv]=ginput;
-		k=convhull(xv,yv);	
+		k=convhull(xv,yv);
 		hold on;
 		plot(xv(k),yv(k),'b-','linewidth',1.25);
 		choice=inpolygon(FEATURES(:,viewdim(1)),FEATURES(:,viewdim(2)),xv(k),yv(k));
 		LABELS(choice==1)=counter;
-		response2=input('(D)one drawing or (c)ontinue?  ','s');
+		response2=questdlg('Done drawing or keep going?','Keep going?',...
+			'Done','Continue','Done');
 
-		switch lower(response2)
+		switch lower(response2(1))
 			case 'd'
 				break;
 			case 'c'
@@ -257,12 +295,12 @@ if draw_mode
 else
 	ndims=3;
 	LABELS=CLUSTERFUN(FEATURES(:,dim),clusternum);
-
 end
 
 set(pop_up_choice,'value',1);
 set(pop_up_choice,'string',[1:length(unique(LABELS))]);
 change_plot();
+set(status_text,'string','Status:  Clustering complete, waiting for input...');
 
 end
 
