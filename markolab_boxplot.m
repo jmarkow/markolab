4ec1bc4da3855882321763ea48fe1114be05b82f
Jeffrey Markowitz <jmarkow@cns.bu.edu>
2016-03-05 17:34:07 -0500

    Boxplot mod

--- markolab_boxplot.m
+++ markolab_boxplot.m
@@ -21,6 +21,8 @@ bunching_offset=2;
 feature_idx=[];
 box_width=.8;
 linewidth=.5
+whiskers=0;
+prctiles=[25 75];
 
 nparams=length(varargin);
 
@@ -48,10 +50,14 @@ for i=1:2:nparams
 			bunching_offset=varargin{i+1};
 		case 'box_width'
 			box_width=varargin{i+1};
-        case 'box_color'
-            box_color=varargin{i+1};
-        case 'med_color'
-            med_color=varargin{i+1};
+    case 'box_color'
+      box_color=varargin{i+1};
+    case 'med_color'
+      med_color=varargin{i+1};
+    case 'whiskers'
+      whiskers=varargin{i+1};
+		case 'prctiles'
+			prctiles=varargin{i+1};
 	end
 end
 
@@ -102,31 +108,46 @@ if ~isempty(bunching)
 	pos_idx(1:bunching:length(pos_idx))=bunching_offset;
 end
 pos_idx=cumsum([pos_idx]);
-boxplot(DATA,GRPS,'positions',pos_idx,'width',box_width,'boxstyle','outline')
+boxplot(DATA,GRPS,'positions',pos_idx,'width',box_width,'boxstyle','outline');
+grpids=unique(GRPS);
 hold on;
 
 WHISK_HANDLE=findobj(gcf,'-regexp','Tag','\w*Whisker');
 BOX_HANDLE=findobj(gca,'tag','Box');
 MED_HANDLE=findobj(gca,'tag','Median');
-
 set(WHISK_HANDLE,'LineStyle','-');
-if fill_box
 
+xidx=nan(1,length(BOX_HANDLE));
+for i=1:length(BOX_HANDLE)
+	xidx(i)=median(get(BOX_HANDLE(i),'xdata'));
+end
+[~,sorting]=sort(xidx);
+
+if fill_box
 	for i=1:nfeatures
 		idx=find(feature_idx==uniq_features(i));
-		for j=1:length(idx)	
-            if ischar(box_color)
-                patch(get(BOX_HANDLE(idx(j)),'XData'),get(BOX_HANDLE(idx(j)),'YData'),ones(1,3),'edgecolor','k','linewidth',linewidth,'facecolor',box_color);
-            else
-                patch(get(BOX_HANDLE(idx(j)),'XData'),get(BOX_HANDLE(idx(j)),'YData'),box_color(i,:),'edgecolor','k','linewidth',linewidth);
-            end
+		for j=1:length(idx)
+			% handles are returned in reverse order
+
+			new_ydata=prctile(DATA(GRPS==grpids(sorting(idx(j)))),prctiles)
+			new_ypoints=[new_ydata(1) new_ydata(2) new_ydata(2) new_ydata(1) new_ydata(1)]
+			%new_ypoints=get(BOX_HANDLE(idx(j)),'YData')
+
+			if ischar(box_color)
+        patch(get(BOX_HANDLE(idx(j)),'XData'),new_ypoints,ones(1,3),'edgecolor','k','linewidth',linewidth,'facecolor',box_color);
+      else
+        patch(get(BOX_HANDLE(idx(j)),'XData'),new_ypoints,box_color(j,:),'edgecolor','k','linewidth',linewidth);
+      end
 			plot(get(MED_HANDLE(idx(j)),'XData'),get(MED_HANDLE(idx(j)),'YData'),'k-','color',med_color(i,:),'linewidth',med_width);
-			%set(WHISK_HANDLE([idx(j) idx(j)+ngrps]),'color',box_color(i,:));
+
 		end
 	end
 
 end
 
+if ~whiskers
+    delete(WHISK_HANDLE);
+end
 if ~adjacent
 	h=findobj(gcf,'-regexp','Tag','\w*Adjacent');
 	delete(h);
